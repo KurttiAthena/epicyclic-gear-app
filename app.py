158788f49db910275bbbfab16be7407f48e848aa
KurttiAthena <kurtti.athena@gmail.com>
2026-06-09 19:13:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -245,11 +245,11 @@ def collect_inputs():
                     inputs['z1'] = st.number_input("z1 (sun teeth)", value=73)
                     inputs['z2'] = st.number_input("z2 (planet teeth)", value=26)
                     inputs['z3'] = st.number_input("z3 (ring teeth)", value=125)
-                    inputs['beta_deg'] = st.number_input("Helix angle beta [deg]", value=0.0)
                     inputs['aw_mm'] = st.number_input("Center distance aw [mm]", value=99.0)
                     inputs['x1'] = st.number_input("Profile shift x1", value=0.0)
                 with g2:
                     inputs['m0_mm'] = st.number_input("Module m0 [mm]", value=2.0)
+                    inputs['beta_deg'] = st.number_input("Helix angle beta [deg]", value=0.0)
                     inputs['b_face_mm'] = st.number_input("Face width b [mm]", value=25.0)
                     ns_pa = st.checkbox("Non-standard pressure angle?", False)
                     inputs['alpha_deg'] = st.number_input("Custom alpha [deg]", value=23.04, disabled=not ns_pa)
